c31d0c4f2ff1f4068c6df4718b389ac45af150d4
Naoya Ikushima <alexnaoya@gmail.com>
2021-10-01 09:53:02 +0900

    TextRecognition

--- app/src/main/java/jp/ac/doshisha/mikilab/spajamapp/ThirdFragment.kt
+++ app/src/main/java/jp/ac/doshisha/mikilab/spajamapp/ThirdFragment.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.ImageDecoder
+import android.net.Uri
 import android.os.Bundle
 import android.util.Base64
 import android.util.Log
@@ -15,13 +16,16 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.gms.tasks.Task
 import com.google.firebase.functions.FirebaseFunctions
 import com.google.gson.*
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.japanese.JapaneseTextRecognizerOptions
 import jp.ac.doshisha.mikilab.spajamapp.databinding.FragmentThirdBinding
 import java.io.ByteArrayOutputStream
+import java.io.IOException
 
 
 class ThirdFragment : Fragment()  {
@@ -31,18 +35,25 @@ class ThirdFragment : Fragment()  {
     private val binding get() = _binding!!
     private lateinit var functions: FirebaseFunctions
     private var intentThrower: ActivityResultLauncher<Intent>? = null
+    private var _uri: Uri? = null
+    private var  image: InputImage? = null
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         intentThrower = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
             if (it?.resultCode == Activity.RESULT_OK) {
                 it.data?.let { data: Intent ->
                     val uri = data.toUri(Intent.URI_INTENT_SCHEME)
                     Log.v(this.tag, uri)
+                    _uri = data.data
+                    val img : ImageView? = view?.findViewById(R.id.imgView)
+                    if (uri != null){
+                        img?.setImageURI(_uri)
+                    }
+
                 }
             }
         }
@@ -96,84 +107,56 @@ class ThirdFragment : Fragment()  {
                 JsonParser.parseString(Gson().toJson(result))
             }
     }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        val img : ImageView? = view?.findViewById(R.id.imgView)
-        if (resultCode != AppCompatActivity.RESULT_OK || READ_REQUEST_CODE == requestCode) {
-            val resultUri = data?.data
-            val contentResolver = requireActivity().contentResolver
-            val source = ImageDecoder.createSource(contentResolver, data?.data!!)
-            var bitmap = ImageDecoder.decodeBitmap(source)
-            // Scale down bitmap size
-            bitmap = scaleBitmapDown(bitmap, 640)// Convert bitmap to base64 encoded string
-            val byteArrayOutputStream = ByteArrayOutputStream()
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
-            val imageBytes: ByteArray = byteArrayOutputStream.toByteArray()
-            val base64encoded = Base64.encodeToString(imageBytes, Base64.NO_WRAP)
-
-            // Create json request to cloud vision
-            val request = JsonObject()
-            // Add image to request
-            val image = JsonObject()
-            image.add("content", JsonPrimitive(base64encoded))
-            request.add("image", image)
-
-            annotateImage(request.toString())
-                .addOnCompleteListener { task ->
-                    if (!task.isSuccessful) {
-                        // Task failed with an exception
-                        val landmarkName = "No Landmark"
-                        val confidence = 0
-                        val NameText : TextView? = view?.findViewById(R.id.LandmarkName)
-                        val ValConText : TextView? = view?.findViewById(R.id.Confidence)
-                        NameText?.setText(landmarkName)
-                        ValConText?.setText(confidence.toString())
-                    } else {
-                        // Task completed successfully
-                        var landmarkName =""
-                        var score = ""
-                        for (label in task.result!!.asJsonArray[0].asJsonObject["landmarkAnnotations"].asJsonArray) {
-                            val labelObj = label.asJsonObject
-                            landmarkName = labelObj["description"].toString()
-                            score = labelObj["score"].toString()
-                        }
-
-                        val NameText : TextView? = view?.findViewById(R.id.LandmarkName)
-                        val ValConText : TextView? = view?.findViewById(R.id.Confidence)
-                        NameText?.setText(landmarkName)
-                        ValConText?.setText(score)
-
-                    }
-                }
-//            //Add features to the request
-//            val feature = JsonObject()
-//            feature.add("maxResults", JsonPrimitive(5))
-//            feature.add("type", JsonPrimitive("LANDMARK_DETECTION"))
-//            val features = JsonArray()
-//            features.add(feature)
-//            request.add("features", features)
-            if(null != resultUri)
-            {
-                if (img != null) {
-                    img.setImageURI(resultUri)
-                }
-            }
+    private fun getlandmark() {
+//        val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+        val recognizer = TextRecognition.getClient(JapaneseTextRecognizerOptions.Builder().build())
+        val contentResolver = requireActivity().contentResolver
+        val source = ImageDecoder.createSource(contentResolver, _uri!!)
+        var bitmap = ImageDecoder.decodeBitmap(source)
+        bitmap = scaleBitmapDown(bitmap, 640)// Convert bitmap to base64 encoded string
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
+        try{
+            image = InputImage.fromFilePath(context, _uri)
+        }catch (e: IOException){
+            e.printStackTrace()
         }
-    }
+        val result = recognizer.process(image)
+            .addOnSuccessListener() { visionText ->
+                // Task completed successfully
+                val resultText = visionText.text
+                val NameText : TextView? = view?.findViewById(R.id.RecText)
+                NameText?.setText(resultText)
+                for (block in visionText.textBlocks) {
+                    val blockText = block.text
 
+                }
+
+            }
+            .addOnFailureListener { e ->
+                // Task failed with an exception
+                // ...
+            }
+
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        var nextbtn : Button = view.findViewById(R.id.button_select)
+        val nextbtn : Button = view.findViewById(R.id.button_select)
+        val getlmbtn : Button = view.findViewById(R.id.landmarkrecog)
         nextbtn.background = resources.getDrawable(R.drawable.btn, null)
 
         nextbtn.setOnClickListener {
             selectPhoto()
         }
+        getlmbtn.setOnClickListener {
+            getlandmark()
+        }
 
     }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
