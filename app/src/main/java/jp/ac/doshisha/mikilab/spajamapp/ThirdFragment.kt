3ffda19747ac9713ac7b78a450094d99b08cf75e
ryo-jpg <73480314+ryo-jpg@users.noreply.github.com>
2021-10-02 23:07:27 +0900

    added image path

--- app/src/main/java/jp/ac/doshisha/mikilab/spajamapp/ThirdFragment.kt
+++ app/src/main/java/jp/ac/doshisha/mikilab/spajamapp/ThirdFragment.kt
@@ -45,19 +45,77 @@ class ThirdFragment : Fragment()  {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        intentThrower = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            if (it?.resultCode == Activity.RESULT_OK) {
-                it.data?.let { data: Intent ->
-                    val uri = data.toUri(Intent.URI_INTENT_SCHEME)
-                    Log.v(this.tag, uri)
-                    _uri = data.data
-                    val img : ImageView? = view?.findViewById(R.id.imgView)
-                    if (uri != null){
-                        img?.setImageURI(_uri)
-                    }
-                }
-            }
-        }
+//        intentThrower = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+//            if (it?.resultCode == Activity.RESULT_OK) {
+//                it.data?.let { data: Intent ->
+//                    val uri = data.toUri(Intent.URI_INTENT_SCHEME)
+//                    Log.v(this.tag, uri)
+//                    _uri = data.data
+//                    val img : ImageView? = view?.findViewById(R.id.imgView)
+//                    if (uri != null){
+//                        img?.setImageURI(_uri)
+//                    }
+//                }
+//            }
+//        }
+        val dogs = listOf(
+            Image(R.drawable.d1),
+            Image(R.drawable.d2),
+            Image(R.drawable.d3),
+            Image(R.drawable.d4),
+            Image(R.drawable.d5),
+            Image(R.drawable.d6),
+            Image(R.drawable.d7),
+            Image(R.drawable.d8),
+            Image(R.drawable.d9),
+            Image(R.drawable.d10),
+            Image(R.drawable.d11),
+            Image(R.drawable.d12),
+            Image(R.drawable.d13),
+            Image(R.drawable.d14),
+            Image(R.drawable.d15),
+        )
+        val osakas = listOf(
+            Image(R.drawable.o1),
+            Image(R.drawable.o2),
+            Image(R.drawable.o3),
+            Image(R.drawable.o4),
+            Image(R.drawable.o5),
+            Image(R.drawable.o6),
+            Image(R.drawable.o7),
+            Image(R.drawable.o8),
+            Image(R.drawable.o9),
+            Image(R.drawable.o10),
+            Image(R.drawable.o11),
+            Image(R.drawable.o12),
+            Image(R.drawable.o15),
+            Image(R.drawable.o16),
+            Image(R.drawable.o17),
+            Image(R.drawable.o18),
+        )
+
+        val ramens = listOf(
+            Image(R.drawable.r1),
+            Image(R.drawable.r2),
+            Image(R.drawable.r3),
+            Image(R.drawable.r4),
+            Image(R.drawable.r5),
+            Image(R.drawable.r6),
+            Image(R.drawable.r7),
+            Image(R.drawable.r8),
+            Image(R.drawable.r9),
+            Image(R.drawable.r10),
+            Image(R.drawable.r11),
+            Image(R.drawable.r12),
+            Image(R.drawable.r13),
+            Image(R.drawable.r14),
+            Image(R.drawable.r15),
+        )
+
+        val viewed_images = mutableListOf<Image>()
+        viewed_images.addAll(osakas)
+        viewed_images.addAll(ramens)
+        viewed_images.addAll(dogs)
 
         _binding = FragmentThirdBinding.inflate(inflater, container, false)
         return binding.root
